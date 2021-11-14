d9322bfd0477a4a29a839f2f1d1926b3d4d0c57e
Bernardo Tuso <btuso.95@gmail.com>
2021-11-13 23:20:31 -0300

    Improved game loop using RequestAnimationFrame, debug shader shows wall planes

--- engine.js
+++ engine.js
@@ -1,59 +1,33 @@
 let Input;
 let Render;
-
-const TARGET_FPS = 60;
-
-async function start() {
-    let killswitch = 0;
-    let previousFrameStart = timeInMilis();
-    const game = new Game();
-
-    game.start();
-    while (true && killswitch < 2000) {
-        let currentFrameStart = timeInMilis();
-        const delta = currentFrameStart - previousFrameStart;
-        
-        Input.pollInputs();
-        game.update(delta / 1000);
-        
-        let s = timeInMilis();
-        window.requestAnimationFrame(() =>
-            Render.draw(game.getCamera(), game.getScene().filter(node => node.shouldBeRendered()))
-        );
-        let f = timeInMilis();
-        
-        // FPS cap
-        await limitFPS(currentFrameStart, TARGET_FPS);
-      //  console.log('Time between frames is: ' + delta + '  frame #'+ killswitch + ",  time spent rendering: "+ (f-s));
-        
-        previousFrameStart = currentFrameStart;
-        killswitch++;
-    }
-    console.log('Done')
-};
-
-function timeInMilis() {
-    return new Date().getTime();
-}
-
-// Ensure FPS are capped at #targetFPS to prevent CPU hogging
-async function limitFPS(frameStart, targetFPS) {
-    const timeUpToNow = timeInMilis() - frameStart;
-    const idle = (1000 / targetFPS) - timeUpToNow;
-    await sleep(idle);
-};
-
-// setTimeout only has 10ms granularity, therefore this can only be used to cap fps to a maximum of around 80 fps
-async function sleep(ms) {
-    return new Promise((resolve) => {
-        setTimeout(resolve, ms);
-      });
-};
+let game;
+let previousFrameStart;
 
 window.onload = function() {
     Render = new RenderingSystem(document, window);
     Input = new InputSystem(window, INPUT_MAP, Render.getScreenSize());
-    start();
+    game = new Game();
+
+    game.start();    
+    previousFrameStart = new Date().getTime();
+    window.requestAnimationFrame(processFrame);
+};
+
+let killswitch = 0;
+function processFrame(currentFrameStart) {
+    const delta = currentFrameStart - previousFrameStart;
+    previousFrameStart = currentFrameStart;
+    
+    Input.pollInputs();
+    game.update(delta / 1000);
+    
+    Render.draw(game.getCamera(), game.getScene().filter(node => node.shouldBeRendered()))
+    
+    console.log('Time between frames is: ' + delta + '  frame #'+ killswitch);
+    
+    killswitch++;
+    if (killswitch < 2000) 
+        window.requestAnimationFrame(processFrame);
 };
 
 // Called from index.html
